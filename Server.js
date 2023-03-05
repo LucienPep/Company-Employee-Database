2cae3435ea37df70b3a6c7cf0620bfac7613647f
LucienPep <xxyyzzoozz.com>
2023-03-05 22:07:02 +1030

    Finished most call functionality and started adding bonus functioanlity / debug and cleaned up code

--- Server.js
+++ Server.js
@@ -12,8 +12,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-var roles = ''
-
 function banner() {
   console.log(`
    ___________________________________________________________ 
@@ -44,10 +42,9 @@ function pageStart(){
           type: 'list',
           message: 'What would you like to do?',
           name: 'mainSelection',
-          choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+          choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Update Employee Manager', 'View Employees by Manager', 'Quit']
       },
-  ])
-  .then((response) => {
+  ]).then((response) => {
       const choice = response.mainSelection
 
       switch (choice) {
@@ -72,6 +69,12 @@ function pageStart(){
           case 'Add Department':
               addDepartment()
               break
+          case 'Update Employee Manager':
+              updateManager()
+              break
+          case 'View Employees by Manager':
+              viewEmployeesByManager()
+              break
           case 'Quit':
             process.exit(1)
         }
@@ -92,7 +95,8 @@ function viewEmployees() {
   FROM employee e
   JOIN main_role ON e.role_id = main_role.id
   JOIN department ON main_role.department_id = department.id
-  LEFT JOIN employee m ON e.manager_id = m.id;
+  LEFT JOIN employee m ON e.manager_id = m.id
+  ORDER BY e.id ASC;
   `, (err, data) => {
     if (err) {
       console.log(err)
@@ -103,10 +107,12 @@ function viewEmployees() {
   });
 
 }
+
 function viewRoles() { 
   db.query(`SELECT main_role.id, main_role.title, department.department_name AS department, main_role.salary
   FROM main_role
-  JOIN department ON main_role.department_id = department.id;`, (err, data) => {
+  JOIN department ON main_role.department_id = department.id
+  ORDER BY main_role.id ASC;`, (err, data) => {
     if (err) {
       console.log(err)
       return;
@@ -117,7 +123,8 @@ function viewRoles() {
 }
 
 function viewDepartment() { 
-  db.query('SELECT * FROM department', (err, data) => {
+  db.query(`SELECT * FROM department
+  ORDER BY department.id ASC;`, (err, data) => {
     if (err) {
       console.log(err)
       return;
@@ -188,6 +195,11 @@ function addEmployeeTwo(role, emp, len){
         }
         const roleID = data[0].id
 
+        if(response.manager == 'null'){
+          const empID = null
+          addEmployeeThree(len, response.firstName, response.lastName, roleID, empID)
+        }else{
+
         var managerArray = []
         managerArray.push(response.manager.split(" "))
 
@@ -197,18 +209,277 @@ function addEmployeeTwo(role, emp, len){
             return;
           }
           const empID = data[0].id
-      
-          db.query(`INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-            VALUES (${len}, '${response.firstName}', '${response.lastName}', ${roleID}, ${empID})` 
+
+          addEmployeeThree(len, response.firstName, response.lastName, roleID, empID)
+        })
+      }
+      })
+    })
+}
+
+function addEmployeeThree(id, first, last, role, emp){
+  db.query(`INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+  VALUES (${id}, '${first}', '${last}', ${role}, ${emp})` 
+  )
+  pageStart()
+}
+
+function addDepartment(){
+  inquirer
+  .prompt([
+      {
+          type: 'input',
+          message: 'What is the name of the department?',
+          name: 'departmentName',
+      },
+    ]).then((response) => {
+      db.query(`INSERT INTO department (department_name)
+            VALUES ('${response.departmentName}')` 
             )
             pageStart()
+    })
+}
+
+function addRole(){
+  db.query(`SELECT department.department_name FROM department`, (err, data) => {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    
+    var depArray = []
+    for(i = 0; i < data.length; i++){
+      depArray.push(data[i].department_name)
+    }
+    addRoleTwo(depArray)
+  })
+}
+
+function addRoleTwo(dep){
+  inquirer
+  .prompt([
+      {
+          type: 'input',
+          message: 'What is the name of the role?',
+          name: 'roleName',
+      },
+      {
+        type: 'input',
+        message: 'What is the salary of the role?',
+        name: 'roleSalary',
+    },
+    {
+      type: 'list',
+      message: 'What department does the role belong to?',
+      name: 'departmentName',
+      choices: dep,
+  },
+  ]).then((response) => {
+    db.query(`SELECT id FROM department WHERE department_name = '${response.departmentName}'`, (err, data) => {
+      if (err) {
+        console.log(err)
+        return;
+      }
+
+      db.query(`INSERT INTO main_role (title, salary, department_id)
+            VALUES ('${response.roleName}', ${response.roleSalary}, ${data[0].id})` 
+            )
+            pageStart()
+    })
+  })
+}
+
+function updateRole(){
+  db.query(`SELECT main_role.title FROM main_role`, (err, data) => {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    
+    var role2Array = []
+    for(i = 0; i < data.length; i++){
+      role2Array.push(data[i].title)
+    }
+    
+    db.query(`SELECT CONCAT (employee.first_name, ' ', employee.last_name) AS employee_name FROM employee`, (err, data) => {
+      if (err) {
+        console.log(err)
+        return;
+      }
+      
+      var employee2Array = []
+      for(i = 0; i < data.length; i++){
+        employee2Array.push(data[i].employee_name)
+      }
+      updateRoleTwo(role2Array, employee2Array,)
+    })
+  })
+}
+
+function updateRoleTwo(role2, emp2){
+  inquirer
+  .prompt([
+      {
+          type: 'list',
+          message: "Which employee's role do you wish to update?",
+          name: 'employeeSelected',
+          choices: emp2,
+      },
+      {
+          type: 'list',
+          message: 'Which role to you wish to assign the selected employee?',
+          name: 'roleSelected',
+          choices: role2,
+      },
+  ]).then((response) => {
+    db.query(`SELECT id FROM main_role WHERE title = '${response.roleSelected}'`, (err, data) => {
+      if (err) {
+        console.log(err)
+        return;
+      }
+      const role2ID = data[0].id
+
+      var employeeArray = []
+      employeeArray.push(response.employeeSelected.split(" "))
+
+      db.query(`SELECT id FROM employee WHERE first_name = '${employeeArray[0][0]}' AND last_name = '${employeeArray[0][1]}'`, (err, data) => {
+        if (err) {
+          console.log(err)
+          return;
+        }
+        const emp2ID = data[0].id
+        
+        db.query(`UPDATE employee SET role_id = ${role2ID} WHERE id = ${emp2ID}`)
+        pageStart()
+      })
+    })
+  })
+}
+
+function updateManager(){
+   db.query(`SELECT CONCAT (employee.first_name, ' ', employee.last_name) AS employee_name FROM employee`, (err, data) => {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    
+    var employee3Array = []
+    for(i = 0; i < data.length; i++){
+      employee3Array.push(data[i].employee_name)
+    }
+    updateManagerTwo(employee3Array)
+  })
+}
+
+function updateManagerTwo(emp3){
+  inquirer
+  .prompt([
+      {
+          type: 'list',
+          message: "Which employee's manager do you wish to update?",
+          name: 'employeeSelected',
+          choices: emp3,
+      },
+      {
+          type: 'list',
+          message: 'Which manager to you wish to assign the selected employee?',
+          name: 'managerSelected',
+          choices: emp3,
+      },
+  ]).then((response) => {
+
+      var employeeArray = []
+      employeeArray.push(response.employeeSelected.split(" "))
+
+      var managerArray = []
+      managerArray.push(response.managerSelected.split(" "))
+
+      db.query(`SELECT id FROM employee WHERE first_name = '${employeeArray[0][0]}' AND last_name = '${employeeArray[0][1]}'`, (err, data) => {
+        if (err) {
+          console.log(err)
+          return;
+        }
+        const emp3ID = data[0].id
+        
+        db.query(`SELECT id FROM employee WHERE first_name = '${managerArray[0][0]}' AND last_name = '${managerArray[0][1]}'`, (err, data) => {
+          if (err) {
+            console.log(err)
+            return;
+          }
+          const manID = data[0].id
+          
+          db.query(`UPDATE employee SET manager_id = ${manID} WHERE id = ${emp3ID}`)
+          pageStart()
         })
       })
     })
 }
 
+function viewEmployeesByManager(){
+  db.query(`SELECT CONCAT (employee.first_name, ' ', employee.last_name) AS employee_name FROM employee`, (err, data) => {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    
+    var employee3Array = []
+    for(i = 0; i < data.length; i++){
+      employee3Array.push(data[i].employee_name)
+    }
+    viewEmployeesByManagerTwo(employee3Array)
+  })
+}
 
+function viewEmployeesByManagerTwo(emp4){
+  inquirer
+  .prompt([
+      {
+          type: 'list',
+          message: "Select manager to view employees?",
+          name: 'managerSelected',
+          choices: emp4,
+      },
+  ]).then((response) => {
 
+    var manager2Array = []
+      manager2Array.push(response.managerSelected.split(" "))
+
+    db.query(`SELECT id FROM employee WHERE first_name = '${manager2Array[0][0]}' AND last_name = '${manager2Array[0][1]}'`, (err, data) => {
+      if (err) {
+        console.log(err)
+        return;
+      }
+      const man2ID = data[0].id
+
+      db.query(`SELECT id FROM employee WHERE manager_id = ${man2ID}`, (err, data) => {
+        if (err) {
+          console.log(err)
+          return;
+        }
+
+        var managerArray = []
+        for(i = 0; i < data.length; i++){
+          managerArray.push(data[i].id)
+        
+        db.query(`SELECT e.id, e.first_name, e.last_name, main_role.title,    department.department_name, main_role.salary, CONCAT (m.first_name, ' ',  m.last_name) AS manager
+          FROM employee e
+          JOIN main_role ON e.role_id = main_role.id
+          JOIN department ON main_role.department_id = department.id
+          LEFT JOIN employee m ON e.manager_id = m.id
+          WHERE e.id = ${managerArray[i]}
+          ORDER BY e.id ASC
+          `, (err, data) => {
+            if (err) {
+            console.log(err)
+            return;
+            }
+            printTable(data)
+          })
+        }
+      })
+    })
+  })
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
